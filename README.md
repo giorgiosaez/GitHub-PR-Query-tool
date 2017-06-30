fc48651527777612c6ce55097c09e117e376891b
Giorgio Saez <giorgio.saez@conexlink.com>
2017-06-30 14:02:50 -0500

    QT-1 README updated & script added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GitHub-PR-Query-tool
 command line utility to get list of all PRs in given time interval 
+
+
+# Usage
+-Windows/Linux:   ghq.py --repo 'giorgiosaez/GitHub-PR-Query-tool' --jira_key "QT-1" --since "3 days"
+-Mac:              ./ghq --repo 'giorgiosaez/GitHub-PR-Query-tool' --jira_key "QT-1" --since "3 days"
---
+++ ghq.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+set PYTHONIOENCODING=UTF-8
+Usage: 
+Windows/Linux:   ghq.py --repo 'giorgiosaez/GitHub-PR-Query-tool' --jira_key "QT-1" --since "3 days"
+Mac:              ./ghq --repo 'giorgiosaez/GitHub-PR-Query-tool' --jira_key "QT-1" --since "3 days"
+"""
+
+import sys, getopt
+import datetime, dateparser
+from github import Github
+from tabulate import tabulate
+from collections import Counter
+import re
+from pandas import DataFrame
+
+def getFilesSummary(fileList):
+    summary = []
+    for prFile in fileList:
+        summary.append( str(prFile.filename.split(".")[-1]))
+    return  re.sub("}|{","",str(dict(Counter(summary))))
+    
+def main(argv):
+    repo = ''
+    jira_key = ''  
+    since = ''
+    try:
+        opts, args = getopt.getopt(argv,"hs:k:r:",["since=","jira_key=","repo="])
+    except getopt.GetoptError:
+        print ('[+] Usage: %s -r <repo> -k <jira_key> -s <since>' % sys.argv[0])
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'ghp.py -r <repo> -k <jira_key> -s <since>'
+            sys.exit()
+        elif opt in ("-r" ,"--repo"):
+            repo = arg
+        elif opt in ("-k", "--jira_key"):
+            jira_key = arg   
+        elif opt in ("-s", "--since"):
+            since = arg
+            if len(since.split(" ")) < 2 or (len(since.split(" ")) >= 2 and 'ago' in since):
+                since = "1 " + since 
+            if 'ago' not in since:
+                since += ' ago'       
+                
+    dateFrom = dateparser.parse(since)
+    dateTo = datetime.date.today()
+    
+    git = Github('42e818da437f7553f45416eb41c4172be353bc0f')
+    gitRepo = git.get_repo('h2oai/h2o-3')
+    pulls = gitRepo.get_pulls(state="all")
+
+    usersTable = []
+    prTable = []
+    for PR in pulls:
+       if PR.created_at > dateFrom:
+           merged = PR.merged_at.strftime("%Y-%m-%d") if isinstance(PR.merged_at, datetime.date) else "---"   
+           timeToMerge = (PR.merged_at-PR.created_at) if PR.merged_at else "---" 
+           offensive = "!!!" if jira_key not in PR.title else ""
+           
+           prTable.append([PR.title,PR.state,PR.created_at.strftime("%Y-%m-%d"), merged, timeToMerge, offensive, getFilesSummary(PR.get_files())])
+           usersTable.append([(PR.user.login), 1 if (PR.state)=="closed" else 0 , 1])
+    
+    #aggreation users table to sum # of PR merged and total merged PR
+    df = DataFrame(data=usersTable,columns=["Authors", "Merged PRs","All PRs"])
+    df1 = df.groupby(df['Authors']).sum()
+
+    print ("*** GitHub repo %s (from %s to %s) ***" % (repo, (dateFrom).strftime("%Y-%m-%d"), str(dateTo)))
+    if prTable:
+        print tabulate(prTable, headers = ["Title", "State", 'Created at', 'Merged at', 'Time to merge', 'Offensive', 'File Stats'])
+        print
+        print "*** Authors ***"
+        print tabulate(df1, headers = ["Authors", "Merged PRs","All PRs"])
+    else:
+        print "Sorry there aren't any Pull Request since " + since
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("[+] Usage: %s -r <repo> -k <jira_key> -s <since>" % sys.argv[0])
+        sys.exit(0)
+    else:
+        main(sys.argv[1:])
